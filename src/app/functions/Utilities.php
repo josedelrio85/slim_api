4ea89808225402f0c25f634bf43c295c176fcd75
Jose del Río <joserio@bysidecar.com>
2018-07-09 14:26:42 +0200

    Utilities 	- Clase en la que se incluirán métodos estáticos para utilizar en las funciones en las que se interactúa con la bd, ya sea en select como acciones CRUD
    	- get_format_prepared_sql($array)
    
    		Función para genera un array con los tipos de parámetros que se van a pasar a la instrucción prepare. Por ahora solo hay soporte para los tipos
    		int y string, que son los más útiles
    
    	- array_insert($array, elements, $position)
    
    		Función para insertar elementos en un array

---
+++ src/app/functions/Utilities.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Functions;
+
+/**
+ * Description of Utilities
+ *
+ * @author Jose
+ */
+class Utilities {
+    
+ /*
+    Array insert
+    @array      the array to add an element to
+    @element    the element to add to the array
+    @position   the position in the array to add the element
+ */
+    public static function array_insert($array, $element, $position) {
+        // if the array is empty just add the element to it
+        if(empty($array)) {
+            $array[] = $element;
+        // if the position is a negative number
+        } elseif(is_numeric($position) && $position < 0) {
+            // if negative position after count
+            if(count($array) + $position < 0) {
+                $position = 0;
+            } else {
+                $position = count($array) + $position;
+            }
+            // try again with a positive position
+            $array = array_insert($array, $element, $position);
+        // if array position already set
+        } elseif(isset($array[$position])) {
+            // split array into two parts
+            $split1 = array_slice($array, 0, $position, true);
+            $split2 = array_slice($array, $position, null, true);
+            // add new array element at between two parts
+            $array = array_merge($split1, array($position => $element), $split2);
+        // if position not set add to end of array
+        } elseif(is_null($position)) {
+            $array[] = $element;
+        // if the position is not set
+        } elseif(!isset($array[$position])) {
+            $array[$position] = $element;
+        }
+        // clean up indexes
+        $array = array_values($array);
+        return $array;
+    }
+    
+/*
+    Genera un array con los formatos necesarios en función 
+    del tipo de variable de cada indice del array
+    @array      el array de parametros en función del cual habrá 
+                que generar los parámetros correspondientes
+ */    
+    public static function get_format_prepared_sql($array){
+        $salida = array();
+        foreach($array as $key => $value){
+            if(is_int($value)){
+                array_push($salida, "%d");
+            }else if(is_string($value)){
+                array_push($salida, "%s");
+            }      
+        }
+        return $salida;
+    }
+}
+
+
+//class objetoDatos {
+//    private $orden;
+//    private $valor;
+//    
+//    function getOrden() {
+//        return $this->orden;
+//    }
+//
+//    function getValor() {
+//        return $this->valor;
+//    }
+//
+//    function setOrden($orden) {
+//        $this->orden = $orden;
+//    }
+//
+//    function setValor($valor) {
+//        $this->valor = $valor;
+//    }
+//    
+//       
+//    public function setPar($valor, $orden){
+//        $this->setValor($valor);
+//        $this->setOrden($orden);
+//    }
+//    
+//    public function getArray(){
+//        return (array)$this;
+//    }
+//    
+//}

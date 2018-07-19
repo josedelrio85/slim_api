1007f16f5510965e6e98dbee7cfabdbccab579ea
Jose del Río <joserio@bysidecar.com>
2018-07-19 14:42:29 +0200

    -Creación del group "RCable". Se incluye en este grupo los métodos /incomingC2c y consultaTimeTableC2C

--- src/routes.php
+++ src/routes.php
@@ -56,93 +56,95 @@ $app->post('/prueba', function (Request $request, Response $response, array $arg
 });     
 
 
-/*
- * Función para gestionar la info asociada a un evento C2C para LP de RCable. Devuelve un array JSON {result:boolean, message:objConexion}
- * -> JSON entrada:
- *  {
- *    "phone": "XXXXXX",
- *    "url": "XXXX",
- * }
- *   */
-$app->post('/incoming_C2C_RCable', function (Request $request, Response $response, array $args){
+$app->group('/RCable/', function(){
     
-    $this->logger->info("WS incoming C2C RCable");
-    
-    if($request->isPost()){
-        $data = $request->getParsedBody();
-        $phone = $data['phone'];
-        
-        $serverParams = $request->getServerParams();
-        $url = "????";
-        if(array_key_exists("HTTP_REFERER", $serverParams)){
-            $url = $serverParams["HTTP_REFERER"];            
-        }
-        $ip = $serverParams["REMOTE_ADDR"];
-        
-        $conn = $this->db_webservice;
+    /*
+     * Función para gestionar la info asociada a un evento C2C para LP de RCable. Devuelve un array JSON {result:boolean, message:objConexion}
+     * -> JSON entrada:
+     *  {
+     *    "phone": "XXXXXX",
+     *    "url": "XXXX",
+     * }
+     *   */
+    $app->post('/incomingC2C', function (Request $request, Response $response, array $args){
 
-        $diaSemana = intval(date('N'));
-        $horaActual = date('H:i');
-        $datos = ["sou_id" => 6, "hora" => $horaActual, "num_dia" => $diaSemana];
-        $consTimeTable = $this->funciones->consultaTimeTableC2C($datos,$conn);
-        $type = 9;        
-        if(is_array($consTimeTable)){
-            $type = 1;
-        }
-        
-        $datos = ["lea_phone" => $phone,
-            "lea_url" => $url,
-            "lea_ip" => $ip,
-            "lea_destiny" => "TEST",
-            "sou_id" => 5,
-            "leatype_id" => $type];
-        
-        if(array_key_exists('test', $data)){
-            $datos["lea_status"] = "TEST";
-        }
-        
-        $formato = $this->utilities->get_format_prepared_sql($datos);
-        $query = $conn->insertStatement("leads",$datos,$formato);
-	$sp = 'CALL wsInsertLead("'.$phone.'", "'.$query.'");';
+        $this->logger->info("WS incoming C2C RCable");
 
-	$result = $conn->Query($sp);
-        
-        if($conn->AffectedRows() > 0){
-            //sustituir llamada
-//            exec("php /var/www/html/Leontel/RCable/sendLeadToLeontel.php >/dev/null 2>&1 &");
+        if($request->isPost()){
+            $data = $request->getParsedBody();
+            $phone = $data['phone'];
 
-            exit(json_encode(['success'=> true, 'message'=> $result->fetch_assoc()]));
+            $serverParams = $request->getServerParams();
+            $url = "????";
+            if(array_key_exists("HTTP_REFERER", $serverParams)){
+                $url = $serverParams["HTTP_REFERER"];            
+            }
+            $ip = $serverParams["REMOTE_ADDR"];
 
-        }else{
-            exit(json_encode(['success'=> false, 'message'=> 'KO-'.$conn->LastError()]));       
+            $conn = $this->db_webservice;
+
+            $diaSemana = intval(date('N'));
+            $horaActual = date('H:i');
+            $datos = ["sou_id" => 6, "hora" => $horaActual, "num_dia" => $diaSemana];
+            $consTimeTable = $this->funciones->consultaTimeTableC2C($datos,$conn);
+            $type = 9;        
+            if(is_array($consTimeTable)){
+                $type = 1;
+            }
+
+            $datos = ["lea_phone" => $phone,
+                "lea_url" => $url,
+                "lea_ip" => $ip,
+                "lea_destiny" => "TEST",
+                "sou_id" => 5,
+                "leatype_id" => $type];
+
+            if(array_key_exists('test', $data)){
+                $datos["lea_status"] = "TEST";
+            }
+
+            $formato = $this->utilities->get_format_prepared_sql($datos);
+            $query = $conn->insertStatement("leads",$datos,$formato);
+            $sp = 'CALL wsInsertLead("'.$phone.'", "'.$query.'");';
+
+            $result = $conn->Query($sp);
+
+            if($conn->AffectedRows() > 0){
+                //sustituir llamada
+    //            exec("php /var/www/html/Leontel/RCable/sendLeadToLeontel.php >/dev/null 2>&1 &");
+
+                exit(json_encode(['success'=> true, 'message'=> $result->fetch_assoc()]));
+
+            }else{
+                exit(json_encode(['success'=> false, 'message'=> 'KO-'.$conn->LastError()]));       
+            }
         }
-    }
+    });
+
+    /*
+     * Función para obtener el horario de atención en C2C. Devuelve un array JSON {result:boolean, data:JSONArray}
+     * -> JSON entrada:
+     *  {
+     *    "sou_id": "6",
+     *    "num_dia": "2",
+     *    "hora": "16:00"
+     * }
+     *   */
+    $app->post('/consultaTimetableC2C', function(Request $request, Response $response, array $args){
+
+        if($request->isPost()){
+            $data = $request->getParsedBody();
+
+            $db = $this->db_webservice;
+            $elements = $this->funciones->consultaTimeTableC2C($data,$db);
+            if(is_array($elements)){
+                exit(json_encode(['success'=> true, 'data' => $elements]));	
+            }else{
+                exit(json_encode(['success'=> false, 'data' => null]));	
+            }
+        } 
+    });
 });
-
-/*
- * Función para obtener el horario de atención en C2C. Devuelve un array JSON {result:boolean, data:JSONArray}
- * -> JSON entrada:
- *  {
- *    "sou_id": "6",
- *    "num_dia": "2",
- *    "hora": "16:00"
- * }
- *   */
-$app->post('/consultaTimetableC2C', function(Request $request, Response $response, array $args){
-   
-    if($request->isPost()){
-        $data = $request->getParsedBody();
-        
-        $db = $this->db_webservice;
-        $elements = $this->funciones->consultaTimeTableC2C($data,$db);
-        if(is_array($elements)){
-	    exit(json_encode(['success'=> true, 'data' => $elements]));	
-	}else{
-	    exit(json_encode(['success'=> false, 'data' => null]));	
-	}
-    } 
-});
-
 
 
 

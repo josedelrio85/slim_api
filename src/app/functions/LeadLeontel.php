b9347e361082b0deec5cb68491030e933815ddfb
Jose del Río <joserio@bysidecar.com>
2018-07-26 11:44:41 +0200

    Se añade las diferencias que existen entre los diferentes métodos sendLeadToFoobartel existentes para los diferentes tipos de source que existen para Creditea.
    Básicamente, la única diferencia que existe, aparte del sou_id, son los campos que se recuperan en la query, y el contenido del array lead que se envía al WS Soap. Excepto en Creditea Stand, que además se filtra por leatype_id.

---
+++ src/app/functions/LeadLeontel.php
@@ -0,0 +1,261 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Functions;
+
+use SoapClient;
+use App\Libraries\UtilitiesConnection;
+/**
+ * Description of LeadLeontel
+ *
+ * @author Jose
+ */
+class LeadLeontel {
+    
+    private static $locationWs = "http://192.168.50.102/webservice/index.php";
+    private static $uriWs = "http://wsLeads";
+    
+    public static function sendLead($data, $db){
+               
+        if(array_key_exists('sou_id', $data)){
+            
+            $datos = [
+                0 => "LEONTEL",
+                1 => NULL,
+                2 => NULL,
+                3 => $data['sou_id'],
+            ];
+/*  
+ * webservice
+sou_id                              sou_idcrm
+1	CREDITEA ABANDONADOS        2
+2	CREDITEA STAND              3
+4	CREDITEA TIMEOUT            5
+9	CREDITEA END TO END         13
+10	CREDITEA FB                 14
+11	CREDITEA RASTREATOR         15
+		
+ * crmti
+2	CREDITEA ABANDONOS          
+3	CREDITEA STAND	
+5	CREDITEA TIMEOUT	
+9	CREDITEA RECEPCION	
+10	CREDITEA RECEPCION NO CLI	
+13	CREDITEA END TO END	
+14	CREDITEA FB	
+15	CREDITEA RASTREATOR	
+17	CREDITEA DISPOSICION DE EFECTIVO	
+		
+*/		
+            switch($data["sou_id"]){
+                case 1:
+                    //Abandonos
+                    $querySource = "l.lea_surname,"
+                        . "l.lea_aux2 dninie,"
+                        . "UPPER(l.lea_aux3) asnef,"
+                        . "l.lea_url,"
+                        . "l.lea_ip";
+                    break;
+                case 9:
+                    //EndToEnd
+                    $querySource = "l.lea_mail,"
+                        . "l.lea_url,"
+                        . "l.lea_aux1,"
+                        . "l.lea_aux2 ";
+                    break;
+                case 10:
+                    //FB
+                    $querySource = "l.lea_mail,"
+                        . "l.lea_ip";
+                    break;
+                case 11:
+                    //Rastreator
+                    $querySource = "l.lea_aux1,"
+                        . "l.lea_aux2,"
+                        . "l.lea_aux3,"
+                        . "l.lea_aux4";
+                    break;
+                case 2:
+                    $querySource = "l.lea_surname,"
+                        . "l.lea_aux2 dninie,"
+                        . "UPPER(l.lea_aux3) asnef,"
+                        . "l.lea_url,"
+                        . "l.lea_ip";
+                    $tam = count($datos);
+                    $datos[$tam] = $data["leatype_id"];
+                    break;                    
+                default;
+            }
+            
+            $query = "SELECT "
+                . "l.lea_id,"
+                . "s.sou_idcrm 'source',"
+                . "lt.leatype_idcrm 'type',"
+                . "l.lea_phone,"
+                . "l.lea_name,";
+            
+            $query .= $querySource;
+
+            $queryFromWhere = " FROM webservice.leads l "
+                . "INNER JOIN webservice.sources s ON l.sou_id = s.sou_id "
+                . "INNER JOIN webservice.leadtypes lt ON l.leatype_id = lt.leatype_id "
+                . "WHERE "
+                . "l.lea_destiny = ? "
+                . "AND l.lea_extracted <=> ? "
+                . "AND l.lea_status <=> ? "
+                . "AND l.sou_id = ? ";
+            
+            if($data["sou_id"] == 2){
+                $queryFromWhere .= " AND l.leatype_id = ? ";
+            }
+            
+            $query .= $queryFromWhere; 
+            $query .= " ORDER BY l.lea_id DESC LIMIT 1;";
+            
+            $format = UtilitiesConnection::getFormatPreparedSql($datos);
+        
+            $r = $db->selectPrepared($query, $datos, $format);
+            
+            if(!is_null($r)){
+                
+                $id_origen_leontel = $r[0]->source;
+		$id_tipo_leontel = $r[0]->type;
+		$lea_id = $r[0]->lea_id;
+                $phone = $r[0]->lea_phone;
+                $nombre = $r[0]->lea_name;
+
+                switch($data["sou_id"]){
+                    case 1:
+                        //Abandonos
+                        $apellidos = $r[0]->lea_surname;
+        		$dninie = $r[0]->lea_aux1;
+                        $url = $r[0]->lea_url;
+                        $ip = $r[0]->lea_ip;
+                        $asnef = $r[0]->asnef;
+                        
+                        $lead = [
+                            'TELEFONO' => $phone,
+                            'nombre' => $nombre,
+                            'apellido1' => $apellidos,
+                            'dninie' => $dninie,
+                            'asnef' => $asnef,
+                            'url' => $url,
+                            'ip' => $ip,
+                            'wsid' => $lea_id
+                        ];
+                        break;
+                    case 9:
+                        //EndToEnd
+                        $dninie = $r[0]->lea_aux1;
+                        $url = $r[0]->lea_url;
+                        $cantidadSolicitada = $r[0]->lea_aux2;
+
+                        $lead = [
+                                'TELEFONO' => $phone,
+                                'url' => $url,
+                                'wsid' => $lea_id,
+                                'dninie' => $dninie,
+                                'observaciones' => $cantidadSolicitada
+                                //'cantidaddeseada' => $cantidadSolicitada
+                        ];
+                        break;
+                    case 10:
+                        //FB
+                        $email = $r[0]->lea_mail;
+                        $ip = $r[0]->lea_ip;
+
+                        $lead = [
+                                'TELEFONO' => $phone,
+                                'nombre' => $nombre,
+                                'Email' => $email,
+                                'ip' => $ip,
+                                'wsid' => $lea_id
+                        ];
+                        break;
+                    case 11:
+                        //Rastreator
+                        $observaciones = "DNI: ".$r[0]->lea_aux4." Ingresos netos: ".$r[0]->lea_aux3." Tipo contrato: ".$r[0]->lea_aux2." Cantidad solicitada: ".$r[0]->lea_aux1;
+                        $name = $reg['lea_name'];
+
+                        $lead = [
+                                'TELEFONO' => $phone,
+                                'observaciones' => $observaciones,
+                                'nombre' => $name,
+                                'wsid' => $lea_id
+                        ];
+                        break;
+                    case 2:
+                        $apellidos = $reg[0]->lea_surname;
+                        $dninie = $reg[0]->dninie;
+                        $asnef = $reg[0]->asnef;
+                        $url = $reg[0]->lea_url;
+                        $ip = $reg[0]->lea_ip;
+
+                        $lead = [
+                                'TELEFONO' => $phone,
+                                'nombre' => $nombre,
+                                'apellido1' => $apellidos,
+                                'dninie' => $dninie,
+                                'asnef' => $asnef,
+                                'url' => $url,
+                                'ip' => $ip,
+                                'wsid' => $lea_id
+                        ];
+                        break;                    
+                    default;
+                }
+            
+                $ws = self::invokeWSLeontel();
+//                comentado para desarrollo
+//                $retorno = $ws->sendLead($id_origen_leontel, $id_tipo_leontel, $lead);
+                $retorno["success"] = true;                
+                $retorno["id"] = 9999;
+                
+                if($retorno["success"]){
+                    $datos = [
+                            "lea_extracted" => date("Y-m-d H:i:s"),
+                            "lea_crmid" => $retorno["id"],
+                            "lea_status" => "PRUEBA"
+                        ];
+                }else{
+                    $datos = [
+                            "lea_crmid" => "ERROR",
+                            "lea_status" => "ERROR"
+                        ];
+                }
+                
+                $formato = UtilitiesConnection::getFormatPreparedSql($datos);
+                $where = ["lea_id" => $lea_id];
+                $formatoWhere = UtilitiesConnection::getFormatPreparedSql($where);
+                $parametros = UtilitiesConnection::getArrayParametrosUpdate($datos, $formato, $where, $formatoWhere);
+                
+                $result = $db->updatePrepared("webservice.leads", $parametros);               
+                $r = json_decode($result);
+               
+                if($r->success){
+                    exit(json_encode(['success'=> true, 'message'=> $r->message]));
+                }else{
+                    exit(json_encode(['success'=> false, 'message'=> $r->message]));
+                }
+            }
+            exit(json_encode(['success'=> false, 'message'=> 'No results']));
+        }
+    }
+    
+    private static function invokeWSLeontel($params = null){
+        
+        $params = $params ? $params : ["location" => self::$locationWs, "uri" => self::$uriWs];
+
+        $ws = new SoapClient(null,[
+		"location" => $params["location"],
+		"uri" => $params["uri"],
+		"compression" => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP]
+	);
+        return $ws;
+    }
+}

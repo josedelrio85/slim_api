6013b3244fa1c9fd80687edad06a2dca89c058ec
Jose del Río <joserio@bysidecar.com>
2018-11-09 12:55:25 +0100

    Soporte para excepciones personalizado
    Para detectar los errores producidos a la hora de realizar un Query o un Prepare, que era un cristo

---
+++ src/app/libraries/MiExcepcion.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Libraries;
+
+class MiExcepcion extends \Exception
+{
+    // Redefinir la excepción, por lo que el mensaje no es opcional
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        // algo de código
+    
+        // asegúrese de que todo está asignado apropiadamente
+        parent::__construct($message, $code, $previous);
+    }
+
+    // representación de cadena personalizada del objeto
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+
+    public function funcionPersonalizada() {
+        echo "Una función personalizada para este tipo de excepción\n";
+    }
+}

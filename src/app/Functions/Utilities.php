a496aa09ce35d178c8bdc41b467f168444ef982b
Jose del Río <joserio@bysidecar.com>
2019-04-16 18:14:58 +0200

    Created infoLog method. Receives a message to generate a log entry using logger instance created when constructing the utilities instance. If a execption is throwed for the logger entity, it is ignored to allow continue the flow.

--- src/app/Functions/Utilities.php
+++ src/app/Functions/Utilities.php
@@ -15,44 +15,57 @@ namespace App\Functions;
  */
 class Utilities {
 
- /*
-    Array insert
-    @array      the array to add an element to
-    @element    the element to add to the array
-    @position   the position in the array to add the element
- */
-    public static function array_insert($array, $element, $position) {
-        // if the array is empty just add the element to it
-        if(empty($array)) {
-            $array[] = $element;
-        // if the position is a negative number
-        } elseif(is_numeric($position) && $position < 0) {
-            // if negative position after count
-            if(count($array) + $position < 0) {
-                $position = 0;
-            } else {
-                $position = count($array) + $position;
-            }
-            // try again with a positive position
-            $array = array_insert($array, $element, $position);
-        // if array position already set
-        } elseif(isset($array[$position])) {
-            // split array into two parts
-            $split1 = array_slice($array, 0, $position, true);
-            $split2 = array_slice($array, $position, null, true);
-            // add new array element at between two parts
-            $array = array_merge($split1, array($position => $element), $split2);
-        // if position not set add to end of array
-        } elseif(is_null($position)) {
-            $array[] = $element;
-        // if the position is not set
-        } elseif(!isset($array[$position])) {
-            $array[$position] = $element;
-        }
-        // clean up indexes
-        $array = array_values($array);
-        return $array;
-    }
-    
+  private $logger;
 
+  function __construct($logger){
+    $this->logger = $logger;
+  }
+ /*
+   *  Array insert
+   *  @array      the array to add an element to
+   *  @element    the element to add to the array
+   *  @position   the position in the array to add the element
+ */
+  public static function array_insert($array, $element, $position) {
+      // if the array is empty just add the element to it
+      if(empty($array)) {
+          $array[] = $element;
+      // if the position is a negative number
+      } elseif(is_numeric($position) && $position < 0) {
+          // if negative position after count
+          if(count($array) + $position < 0) {
+              $position = 0;
+          } else {
+              $position = count($array) + $position;
+          }
+          // try again with a positive position
+          $array = array_insert($array, $element, $position);
+      // if array position already set
+      } elseif(isset($array[$position])) {
+          // split array into two parts
+          $split1 = array_slice($array, 0, $position, true);
+          $split2 = array_slice($array, $position, null, true);
+          // add new array element at between two parts
+          $array = array_merge($split1, array($position => $element), $split2);
+      // if position not set add to end of array
+      } elseif(is_null($position)) {
+          $array[] = $element;
+      // if the position is not set
+      } elseif(!isset($array[$position])) {
+          $array[$position] = $element;
+      }
+      // clean up indexes
+      $array = array_values($array);
+      return $array;
+  }
+  
+  public function infoLog($message){
+    
+    try{
+      $this->logger->info($message);
+    }catch(\Exception $e){
+      return false;
+    }
+    return true;
+  }
 }

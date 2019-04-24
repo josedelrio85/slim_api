1d26e11dc5a0545571fb3937dbbb361240145c7a
Jose del Río <joserio@bysidecar.com>
2019-04-24 16:35:22 +0200

    minor changes

--- src/app/Middleware/JSONContentMiddleware.php
+++ src/app/Middleware/JSONContentMiddleware.php
@@ -4,21 +4,21 @@ namespace App\Middleware;
 
 class ContentType {
     
-    const _JSON = "application/json";
-    const _FORM_URLENCODED = "application/x-www-form-urlencoded";
-    
-    const _JSON_UTF8 = "application/json; charset=UTF-8";
-    const _FORM_URLENCODED_UTF8 = "application/x-www-form-urlencoded; charset=UTF-8";
-    
-    
-    static function getContentTypes(){
-        return $array = [
-            self::_JSON,
-            self::_FORM_URLENCODED,
-            self::_JSON_UTF8,
-            self::_FORM_URLENCODED_UTF8,
-        ];       
-    }
+  const _JSON = "application/json";
+  const _FORM_URLENCODED = "application/x-www-form-urlencoded";
+  
+  const _JSON_UTF8 = "application/json; charset=UTF-8";
+  const _FORM_URLENCODED_UTF8 = "application/x-www-form-urlencoded; charset=UTF-8";
+  
+  
+  static function getContentTypes(){
+    return $array = [
+      self::_JSON,
+      self::_FORM_URLENCODED,
+      self::_JSON_UTF8,
+      self::_FORM_URLENCODED_UTF8,
+    ];
+  }
 }
 
 /**
@@ -28,50 +28,41 @@ class ContentType {
  */
 class JSONContentMiddleware {
     
-    public function __invoke($request, $response, $next) {
-        /*
-        if (!$request->isXhr()){
-            // X-Requested-With realmente solo da información sobre si el request ha sido realizado desde Ajax, como value debe llegar XMLHttpRequest. 
-            // Valorar si interesa implementarlo. ¿Habrá requests desde clientes, curl, etc?
-            // return $response->withStatus(412, "The precondition given in the request evaluated to false by the server ");
+  public function __invoke($request, $response, $next) {
+    /*
+      if (!$request->isXhr()){
+        // X-Requested-With realmente solo da información sobre si el request ha sido realizado desde Ajax, como value debe llegar XMLHttpRequest. 
+        // Valorar si interesa implementarlo. ¿Habrá requests desde clientes, curl, etc?
+        // return $response->withStatus(412, "The precondition given in the request evaluated to false by the server ");
+      }
+    */
+
+    /*
+     * The following middleware can be used to query Slim’s router and get a list of methods a particular pattern implements.
+     * Here is a complete example application:
+    */
+    if(in_array($request->getContentType(), ContentType::getContentTypes())){
+      // It will add the Access-Control-Allow-Methods header to every request
+      $route = $request->getAttribute("route");
+      $methods = [];
+
+      if (!empty($route)) {
+        $pattern = $route->getPattern();
+        foreach ($this->router->getRoutes() as $route) {
+          if ($pattern === $route->getPattern()) {
+            $methods = array_merge_recursive($methods, $route->getMethods());
+          }
         }
-        */
-        
-        /*
-        if($request->getContentType() != "application/json")        
-            return $response->withStatus(400, "Bad content-type");
-        return $next($request, $response);
-        */
-        
-        /*
-        * The following middleware can be used to query Slim’s router and get a list of methods a particular pattern implements.
-        * Here is a complete example application:
-        */
-        if(in_array($request->getContentType(), ContentType::getContentTypes())){
-            
-            // It will add the Access-Control-Allow-Methods header to every request
-            $route = $request->getAttribute("route");
-            $methods = [];
+        //Methods holds all of the HTTP Verbs that a particular route handles.
+      } else {
+        $methods[] = $request->getMethod();
+      }           
+      
+      $response = $next($request, $response);
 
-            if (!empty($route)) {
-                $pattern = $route->getPattern();
-
-                foreach ($this->router->getRoutes() as $route) {
-                    if ($pattern === $route->getPattern()) {
-                        $methods = array_merge_recursive($methods, $route->getMethods());
-                    }
-                }
-                //Methods holds all of the HTTP Verbs that a particular route handles.
-            } else {
-                $methods[] = $request->getMethod();
-            }           
-            
-            $response = $next($request, $response);
-
-            return $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods));
-            
-        }else{
-            return $response->withStatus(400, $request->getContentType()." Bad content-type");
-        }               
-    }
+      return $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods));        
+    }else{
+      return $response->withStatus(400, $request->getContentType()." Bad content-type");
+    }               
+  }
 }
--- src/app/Middleware/ParsedBodyMiddleware.php
+++ src/app/Middleware/ParsedBodyMiddleware.php
@@ -12,22 +12,20 @@ namespace App\Middleware;
   */
 
 class ParsedBodyMiddleware {
+  /*
+    * withParsedBody() => It completely overwrites the request body with the modified object. 
+    * What you have to pay mind to:
+    * From there on, the request will hold a parsed object as body and on calling $request->getParsedBody() it won't be reparsed, 
+    * if I understand the source correctly on calling $request->getParsedBody() you usually get an associative array if the body was JSON, 
+    * but using the snippet above, the parsed body will be an object instead.
+  */    
+  public function __invoke($request, $response, $next){
+    # inside middleware:
+    $requestbody = $request->getBody();
+    $requestobject = json_decode($requestbody);
+    # validation and modification of $requestobject takes place here
+    $request = $request->withParsedBody($requestobject);
 
-    /*
-     * withParsedBody() => It completely overwrites the request body with the modified object. 
-     * What you have to pay mind to:
-     * From there on, the request will hold a parsed object as body and on calling $request->getParsedBody() it won't be reparsed, 
-     * if I understand the source correctly on calling $request->getParsedBody() you usually get an associative array if the body was JSON, 
-     * but using the snippet above, the parsed body will be an object instead.
-    */
-    
-    public function __invoke($request, $response, $next){
-        # inside middleware:
-        $requestbody = $request->getBody();
-        $requestobject = json_decode($requestbody);
-        # validation and modification of $requestobject takes place here
-        $request = $request->withParsedBody($requestobject);
-
-        return $next($request, $response);
-    }
+    return $next($request, $response);
+  }
 }

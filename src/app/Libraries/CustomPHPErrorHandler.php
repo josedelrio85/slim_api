62f632d0e755eb39dbe81f87986ebf35b195fb8f
Jose del Río <joserio@bysidecar.com>
2019-04-25 13:54:40 +0200

    removed exception_handlerAlarm and curl functions and now they are used from CustomError class as static methods

--- src/app/Libraries/CustomPHPErrorHandler.php
+++ src/app/Libraries/CustomPHPErrorHandler.php
@@ -25,56 +25,12 @@ final class CustomPHPErrorHandler extends \Slim\Handlers\PhpError {
     //Log the message
     // $this->logger->critical($body);
 
-    $this->exception_handlerAlarm($error);
+    CustomErrorHandler::exception_handlerAlarm($error);
     
     return $response
       ->withStatus(500)
       ->withHeader('Content-type', 'application/json')
       ->withBody(new \Slim\Http\Body(fopen('php://temp','r+')))
       ->write($body);
-  }
-
-  public function exception_handlerAlarm($exception){
-    $url = "https://alert.victorops.com/integrations/generic/20131114/alert/2f616629-de63-4162-bb6f-11966bbb538d/test";
-
-    switch(get_class($exception)){
-      case "UnexpectedValueException":
-        $state = "CRITICAL";
-        break;
-      default:
-        $state = "ACKNOWLEDGEMENT";
-        break;
-    }
-
-    $params = [
-      "message_type" => $state,
-      "entity_state" => $state,
-      "entity_id" => "webservice_exception",
-      "entity_display_name" => "webservice_exception",
-      "state_message" => json_encode($exception->__toString()),
-      "state_start_time" => time(),
-    ];
-
-    $result = $this->curl($url, json_encode($params));
-    return $result;
-  }
-
-  private function curl($url, $params){
-    $options = array(
-      CURLOPT_RETURNTRANSFER => true,   // return web page
-      CURLOPT_HEADER         => false,  // don't return headers
-      CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-      CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-      CURLOPT_TIMEOUT        => 120,    // time-out on response
-      CURLOPT_POST           => true,
-      CURLOPT_POSTFIELDS     => $params,
-    ); 
-
-    $ch = curl_init($url);
-    curl_setopt_array($ch, $options);
-    // $content = curl_exec($ch);
-    $content = null;
-    curl_close($ch);
-    return $content;
   }
 }

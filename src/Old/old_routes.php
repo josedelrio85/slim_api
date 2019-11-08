77212663767b6f09aa79db0f44901212eab7f9ad
Jose del Río <joserio@bysidecar.com>
2019-11-08 08:34:47 +0100

    move Creditea code to old_routes file

--- src/Old/old_routes.php
+++ src/Old/old_routes.php
@@ -693,3 +693,427 @@ $app->group('/microsoft', function(){
     }
   });
 });
+
+
+$app->group('/creditea', function(){
+
+  /*
+    * Función para almacenar la información de aquellos leads considerados como no válidos en la LP
+    * (marcaron check Cliente y/o Asnef)
+    * * @JSON entrada:
+    *  {
+    *    "utm_source": "XXXXXX",
+    *    "phone": "XXXX",
+    *    "documento": "XXXXXXX",
+    * 	  "cantidadsolicitada": XXXXXXX,
+    *    "motivo": "XXXXXXX"
+    * }
+    * @JSON salida:
+    *      success:boolean
+    *      message:string
+  */
+  $this->post('/almacenaLeadNoValido', function(Request $request, Response $response, array $args){
+
+    $this->utilities->infoLog("WS C2C Creditea E2E almacena lead no válido.");
+
+    if($request->isPost()){
+
+      $data = $request->getParsedBody();
+
+      list($url, $ip) = $this->funciones->getServerParams($request);
+
+      $sou_id = 9;
+      $leatype_id = 1;     
+      if(property_exists($data, "sou_id")){
+        $sou_id = $data->sou_id;
+      }
+      
+      if(property_exists($data, "web_source")){
+        if($data->web_source == "prestamoscreditea.com" && $sou_id == 9) {
+          $leatype_id = $this->funciones->checkGclid($data) ? 25 : $leatype_id;
+        }
+      }
+
+      $datos = [
+        "lea_destiny" => '',
+        "sou_id" => $sou_id,
+        "leatype_id" => $leatype_id,
+        "utm_source" => array_key_exists("utm_source", $data) ? $data->utm_source : null,
+        "sub_source" => array_key_exists("sub_source", $data) ? $data->sub_source : null,
+        "lea_phone" => $data->phone,
+        "lea_url" => $url,
+        "lea_ip" => $ip,
+        "lea_aux1" => $data->documento,
+        "lea_aux2" => $data->cantidadsolicitada,
+        "lea_aux3" => $data->motivo,
+        "lea_aux4" => array_key_exists("gclid", $data) ? $data->gclid : null,
+      ];
+
+      $db = $this->db_webservice;
+      $parametros = UtilitiesConnection::getParametros($datos,null);
+      $salida = json_decode($db->insertPrepared($this->leads_table, $parametros),true);
+
+      return $response->withJson($salida);
+    }
+  });
+
+  /*
+    * Proceso de validación de lead valido cuando en la LP se marcan como "NO" casillas "Cliente" y "Asnef"
+    * params:
+    * @JSON entrada:
+    *  {
+    *    "utm_source": "XXXXXX",
+    *    "phone": "XXXX",
+    *    "documento": "XXXXXXX",
+    * 	  "cantidadsolicitada": XXXXXXX,
+    *    "motivo": "XXXXXXX"
+    * }
+    * @JSON salida:
+    *      success:boolean
+    *      message:string
+  */
+  $this->post('/validaDniTelf', function(Request $request, Response $response, array $args){
+
+    $this->utilities->infoLog("WS para validacion datos LP Creditea.");
+
+    if($request->isPost()){
+      $data = $request->getParsedBody();
+
+      list($url, $ip) = $this->funciones->getServerParams($request);
+
+      $sou_id = 9;
+      $leatype_id = 1;
+      if(property_exists($data, "sou_id")){
+        $sou_id = $data->sou_id;
+      }
+      if(property_exists($data, "web_source")){
+        if($data->web_source == "prestamoscreditea.com" && $sou_id == 9) {
+          $leatype_id = $this->funciones->checkGclid($data) ? 25 : $leatype_id;
+        }
+      }
+
+      $datosAsnef = [
+        "sou_id" => $sou_id,
+        "documento" => $data->documento,
+        "phone" => $data->phone
+      ];
+
+      $rAsnefPre = json_decode($this->funciones->checkAsnefCrediteaPrev($datosAsnef));
+
+      if(!$rAsnefPre->success){
+        //sou_id de crmti
+        $sou_id_crmti = $this->funciones->getSouIdcrm($sou_id);
+        $datosAsnef["sou_id"] = $sou_id_crmti;
+
+        $rAsnef = json_decode($this->funciones->checkAsnefCreditea($datosAsnef));
+
+        if(!$rAsnef->success){
+
+          $datos = [
+            "lea_destiny" => 'LEONTEL',
+            "sou_id" => $sou_id,
+            "leatype_id" => $leatype_id,
+            "utm_source" => array_key_exists("utm_source", $data) ? $data->utm_source : null,
+            "sub_source" => array_key_exists("sub_source", $data) ? $data->sub_source : null,
+            "lea_phone" => $data->phone,
+            "lea_url" => $url,
+            "lea_ip" => $ip,
+            "lea_aux1" => $data->documento,
+            "lea_aux2" => $data->cantidadsolicitada,
+            "lea_aux4" => array_key_exists("gclid", $data) ? $data->gclid : null,
+          ];
+
+          $setwebservice = $this->settings_db_webservice;
+          $db = new \App\Libraries\Connection($setwebservice);
+          $salida = $this->funciones->prepareAndSendLeadLeontel($datos,$db);
+          $db = null;
+        }else{
+          $salida = json_encode(['success' => false, 'message' => $rAsnef->message]);
+        }
+      }else{
+        $salida = json_encode(['success' => false, 'message' => $rAsnefPre->message]);
+      }
+
+      $test = json_decode($salida);
+
+      if(!$test->success){
+
+        $dataInsert = [
+          "lea_destiny" => '',
+          "sou_id" => $sou_id,
+          "leatype_id" => $leatype_id,
+          "utm_source" => array_key_exists("utm_source", $data) ? $data->utm_source : null,
+          "sub_source" => array_key_exists("sub_source", $data) ? $data->sub_source : null,
+          "lea_phone" => $data->phone,
+          "lea_url" => $url,
+          "lea_ip" => $ip,
+          "lea_aux1" => $data->documento,
+          "lea_aux2" => $data->cantidadsolicitada,
+          "lea_aux3" => $test->message,
+          "lea_aux4" => array_key_exists("gclid", $data) ? $data->gclid : null,
+        ];
+        $this->funciones->sendLeadToWebservice($dataInsert);
+
+        $salida = json_encode(['success' => false, 'message' => $test->message]);
+      }
+    }
+    // $this->utilities->infoLog("salida".$salida);
+    return $response->withJson(json_decode($salida, true));
+  });
+
+  /*
+    * Función para volcado de leads considerados como Pago Recurrente. La idea es que se llame a través de cron.
+    * Ver si es posible realizar esto.
+    * @returns
+    *  @ array que será escrito en log para comprobar qué ids se volcaron. (Sin implementar esto último)
+  */
+  $this->post('/sendLeadLeontelPagoRecurrente', function(Request $request, Response $response, array $args){
+
+    $db = $this->db_crmti;
+    $salida = App\Functions\LeadLeontel::sendLeadLeontelPagoRecurrente("leads", $db, $this->dev);
+
+    $salida = App\Functions\LeadLeontel::sendLeadLeontelPagoRecurrente("cliente", $db, $this->dev);
+
+
+    return $response->withJson(json_decode($salida, true));
+  });
+
+  /*
+    * Method to receive the leads sended by IPF and redirect to Leontel system.
+    * params:
+    *  @ [] json:
+    *  [
+    *      {
+    *          "clientId": "XXXXXX", 
+    *	        "nameId":   "XXXXXX", 
+    *	        "phoneId":  "+34XXXXXX",
+    *          "alternativePhoneId": "",
+    *          "lastStatusId": "Not Started",
+    *	        "productAmountTaken": "1500€ CREDIT_LINE", 
+    *          "channel": "Web",
+    *          "type": "New application",
+    *          "putLeadDate": "",		
+    *	        "application": "A-7590008",
+    *          "latestTaskStatus": "Not Started",
+    *	        "idStatusDate": "2019-02-14T22:35:15.000Z"
+    *      }
+    * ]
+    * 
+    * output:
+    * @ [] json:
+    * [
+    *      {
+    *          "success":  boolean
+    *          "message":  "XXXXXX"
+    *      }
+    * ]
+  */
+  $this->post('/ipf', function(Request $request, Response $response, array $args){
+
+    $this->utilities->infoLog("Method for receiving array of leads from IPF.");
+
+    $results = [];
+
+    if($request->isPost()){
+      $leads = (object) $request->getParsedBody();
+
+      $sou_id = 53;
+      $lea_type = 1;
+
+      list($url, $ip) = $this->funciones->getServerParams($request);
+
+      foreach($leads as $lead) {
+
+        foreach ($lead as $key => $value) {
+          $this->utilities->infoLog($key. " => ".$value);
+        }
+        $this->utilities->infoLog("-----------------");
+
+        $observations = $lead->idStatusDate."---".$lead->application;
+        if($this->funciones->phoneFormatValidator($lead->phoneId)){
+          $phone = $lead->phoneId;
+        }else{
+          $phone = substr($lead->phoneId,3);
+        }
+
+        $datos = [
+          "lea_destiny" => 'LEONTEL',
+          "sou_id" => $sou_id,
+          "leatype_id" => $lea_type,
+          "lea_phone" => $phone,
+          "lea_url" => $url,
+          "lea_ip" => $ip,
+          "lea_aux1" => $lead->nameId,
+          "lea_aux2" => $lead->productAmountTaken,
+          "lea_aux4" => $lead->clientId,
+          "observations" => $observations
+        ];
+        array_push($results, json_decode($this->funciones->prepareAndSendLeadLeontel($datos)));
+      }
+      /*
+          Info recibida						        Campo Leontel						    Campo webservice.lead
+          clientId => 4169626				            Nº Cliente (ncliente)					lea_aux4
+          nameId => 15861419K				            Documento (dninie)					    lea_aux1
+          phoneId => +34522361413			            Telefono (telefono)					    lea_phone
+          alternativePhoneId =>
+          lastStatusId => Not Started
+          application => A-7590009			        Observaciones    (observaciones)		observations
+          productAmountTaken => 1500€ CREDIT_LINE	    Cantidad ofrecida (cantidadofrecida)	lea_aux2
+
+          channel => Web
+          type => New application	
+          putLeadDate => 2019-02-10T16:00:00Z	
+          latestTaskStatus => Not Started	
+
+          idStatusDate => 2019-02-11T14:17:45.000Z    Observaciones (observaciones)			observations
+      */
+    }
+
+    $this->utilities->infoLog("----------RESULTS-------");
+    foreach ($results as $key => $value) {
+      $this->utilities->infoLog($key. " => ".$value->success. " ". $value->message);
+    }
+    $this->utilities->infoLog("----------END RESULTS-------");
+
+    return $response->withJson($results);
+  });
+});
+
+$app->group('/doctordinero', function(){
+
+  /*
+    * Proceso de validación de lead valido cuando en la LP se marcan como "NO" casillas "Cliente" y "Asnef"
+    * params:
+    * @JSON entrada:
+    *  {
+    *    "utm_source": "XXXXXX",
+    *    "phone": "XXXX",
+    *    "documento": "XXXXXXX",
+    * 	  "cantidadsolicitada": XXXXXXX,
+    *    "motivo": "XXXXXXX"
+    * }
+    * @JSON salida:
+    *      success:boolean
+    *      message:string
+  */
+  $this->post('/incomingC2C', function(Request $request, Response $response, array $args){
+
+    $this->utilities->infoLog('WS incoming Doctor Dinero.');
+
+    if($request->isPost()){
+      $data = $request->getParsedBody();
+
+      $datos = array();
+      $salida = array();
+      $salidaTxt = "";
+
+      $datos['telf'] = $data->movil;
+      $datos['dninie'] = $data->dni;
+      $datos['importe'] = $data->importe;
+      $datos['ingresosMensuales'] = $data->ingresosMensuales;
+      $datos['nombre'] = $data->nombre;
+      $datos['apellido1'] =$data->apellido1;
+      $datos['apellido2'] =$data->apellido2;
+      $datos['fechaNacimiento'] = $data->fechaNacimiento;
+      $datos['email'] = $data->email;
+      $datos['cp'] = $data->cp;
+
+      foreach($datos as $key => $value){
+        if(empty($value)){
+          array_push($salida,"KO-notValid_".$key);
+          $salidaTxt .= "KO-notValid_".$key."///";
+        }
+      }
+
+      if(!App\Functions\NifNieCifValidator::isValidIdNumber($data->dni)){
+        array_push($salida,"KO-notValid_dninie");
+        $salidaTxt .= "KO-notValid_dninie///";
+      }
+
+      if (!filter_var($data->email, FILTER_VALIDATE_EMAIL)) {
+        array_push($salida,"KO-notValid_email");
+        $salidaTxt .= "KO-notValid_email///";
+      }
+
+      $valido = empty($salida);
+      $telfValido = App\Functions\Functions::phoneFormatValidator($data->movil);
+
+      $sou_id = 9;
+      $sou_idcrm = $this->funciones->getSouIdcrm($sou_id);
+      $leatype_id = 1;
+
+      $observations = "";
+      foreach($data as $v){
+        $observations .= $v."--";
+      }
+
+      list($url, $ip) = $this->funciones->getServerParams($request);
+
+      $datos = [
+        "lea_destiny" => '',
+        "sou_id" => $sou_id,
+        "leatype_id" => $leatype_id,
+        "utm_source" => array_key_exists("utm_source", $data) ? $data->utm_source : null,
+        "sub_source" => array_key_exists("sub_source", $data) ? $data->sub_source : null,
+        "lea_phone" => $data->movil,
+        "lea_url" => $url,
+        "lea_ip" => $ip,
+        "lea_aux1" => $data->dni,
+        "observations" => $observations
+      ];
+
+      $datosAsnef = [
+        "sou_id" => $sou_id,
+        "documento" => $data->dni,
+        "phone" => $data->movil
+      ];
+
+      if($telfValido){
+
+        $rAsnefPre = json_decode($this->funciones->checkAsnefCrediteaPrev($datosAsnef));
+
+        if(!$rAsnefPre->success){               
+          $datosAsnef["sou_id"] = $sou_idcrm;
+          $rAsnef = json_decode($this->funciones->checkAsnefCreditea($datosAsnef));
+
+          if(!$rAsnef->success){
+            $datos["lea_destiny"] = 'LEONTEL';
+            $datos["lea_aux3"] = "Ok_DoctorDinero";
+
+            $setwebservice = $this->settings_db_webservice;
+            $db = new \App\Libraries\Connection($setwebservice);
+
+            $salida = $this->funciones->prepareAndSendLeadLeontel($datos,$db);
+            if(!$valido){
+              $salida = json_encode(['success' => false, 'message' => 'KO-notValid_params']);;
+            }
+            $db = null;
+          }else{
+            $salida = json_encode(['success' => false, 'message' => $rAsnef->message]);
+          }
+        }else{
+          $salida = json_encode(['success' => false, 'message' => $rAsnefPre->message]);
+        }
+
+        $test = json_decode($salida);
+        if(!$test->success){
+          $datos["lea_destiny"] = '';
+          $datos["lea_aux3"] = $test->message;
+
+          $this->funciones->sendLeadToWebservice($datos);
+
+          $salida = json_encode(['success' => false, 'message' => $test->message]);
+        }
+      }else{
+        //telefono no valido
+        $datos["lea_destiny"] = '';
+        $datos["lea_aux3"] = $data->movil."_notValid";
+
+        $this->funciones->sendLeadToWebservice($datos);
+
+        $salida = json_encode(['success' => false, 'message' => 'KO-notValid_telf']);
+      }
+      return $response->withJson(json_decode($salida, true));
+    }
+  });
+});
